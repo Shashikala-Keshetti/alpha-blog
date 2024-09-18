af3837216f3d51a95411bb15d003387cc04f9937
Shashikala <keshettishashikala@gmail.com>
2024-09-18 17:00:51 +0530

    Add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,10 +29,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
 
-  
 end
